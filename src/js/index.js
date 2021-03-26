15c8fae73ae08f956f9e018c5e4c7ac6997a21a5
Andrii <andrewmosin8@gmail.com>
2021-03-26 20:51:45 +0200

    add new features

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../styles/index.scss';
+import React from "react";
+import {render} from "react-dom";
+import {App} from "./components/App";
+
+// @ts-ignore
+render(
+    <App/>,
+    document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,99 +1,100 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 let mode = "development";
 let target = "web";
 let devtool = "source-map";
 
-if (process.env.NODE_ENV === "production ") {
-  mode = "production";
-  target = "browserslist";
-  devtool = "hidden-nosources-source-map";
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = "browserslist";
+    devtool = "hidden-nosources-source-map";
 }
 
 module.exports = {
-  mode: mode,
-  target: target,
+    mode: mode,
+    target: target,
 
-  entry: "./src/js/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "app.bundle.[contenthash].js",
-    assetModuleFilename: "assets/[name][hash][ext]",
-  },
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.bundle.[contenthash].js",
+        assetModuleFilename: "assets/[name][hash][ext]",
+    },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    mode === "development" && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        mode === "development" && new ReactRefreshWebpackPlugin(), // COMMENT THIS IF BUILD-DEV ERROR
+    ].filter(Boolean),
 
-  module: {
-    rules: [
-      {
-        test: /\.(gif|jpg|png|jpeg|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[hash][ext]",
-        },
-      },
-
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[hash][ext]",
-        },
-      },
-
-      {
-        // Include sass, scss, css
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "",
+    module: {
+        rules: [
+            {
+                test: /\.(gif|jpg|png|jpeg|svg)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[hash][ext]",
+                },
             },
-          },
-          {
-            loader: "css-loader",
-          },
 
-          {
-            loader: "postcss-loader",
-          },
-          {
-            loader: "resolve-url-loader",
-          },
-          {
-            loader: "sass-loader",
-          },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[hash][ext]",
+                },
+            },
+
+            {
+                // Include sass, scss, css
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "",
+                        },
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+
+                    {
+                        loader: "postcss-loader",
+                    },
+                    {
+                        loader: "resolve-url-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+            {
+                // Include ts, tsx, js, and jsx files.
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
         ],
-      },
-      {
-        // Include ts, tsx, js, and jsx files.
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-    ],
-  },
+    },
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-  },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    },
 
-  devtool: devtool,
+    devtool: devtool,
 
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
-  },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: "./dist",
+        hot: true,
+    },
 };
